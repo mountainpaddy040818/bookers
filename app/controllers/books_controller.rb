64c8de48f8674839fb0847ba02cec4d8c19c933d
EC2 Default User <ec2-user@ip-172-31-41-119.ap-northeast-1.compute.internal>
2024-01-08 16:20:09 +0000

    課題２

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,52 @@
+class BooksController < ApplicationController
+  
+  def new
+    @book = Book.new
+  end
+  
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = "successfully"
+      redirect_to book_path(@book.id) 
+    else
+      flash.now[:alert] = "error"
+      render :new
+    end 
+  end 
+  
+  def index
+    @books = Book.all
+  end
+
+  def show
+    @book = Book.find(params[:id]) 
+  end
+
+  def edit
+    @book = Book.find(params[:id]) 
+  end
+  
+  def back
+    @book = Book.find(params[:id]) 
+  end 
+  
+  def update
+    book = Book.find(params[:id]) 
+    book.update(book_params)
+    redirect_to book_path(book.id) 
+  end 
+  
+  def destroy
+    book = Book.find(params[:id]) 
+    book.destroy
+    redirect_to '/books'
+  end 
+  
+  private
+  
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end 
+  
+end
