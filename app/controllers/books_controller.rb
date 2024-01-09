45869eda42fd8cd56f2a100eb0d76bfc41d599fd
EC2 Default User <ec2-user@ip-172-31-41-119.ap-northeast-1.compute.internal>
2024-01-09 08:21:36 +0000

    エラー処理完了

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,52 +1,53 @@
 class BooksController < ApplicationController
-  
-  def new
-    @book = Book.new
-  end
-  
+
+
   def create
     @book = Book.new(book_params)
     if @book.save
-      flash[:notice] = "successfully"
-      redirect_to book_path(@book.id) 
+      flash[:notice] = "Book was successfully updated."
+      redirect_to book_path(@book.id)
     else
-      flash.now[:alert] = "error"
-      render :new
-    end 
-  end 
-  
+      @books = Book.all
+      flash.now[:alert] = "error prohibited this book from being saved:"
+      render :index
+    end
+  end
+
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def show
-    @book = Book.find(params[:id]) 
+    @book = Book.find( params[:id] )
   end
 
   def edit
-    @book = Book.find(params[:id]) 
+    @book = Book.find( params[:id] )
   end
-  
-  def back
-    @book = Book.find(params[:id]) 
-  end 
-  
+
   def update
-    book = Book.find(params[:id]) 
-    book.update(book_params)
-    redirect_to book_path(book.id) 
-  end 
-  
+    @book = Book.find( params[:id] )
+    @book.update(book_params)
+     if @book.update(book_params)
+      flash[:notice] = "Book was successfully updated."
+      redirect_to book_path(@book.id)
+    else
+      flash.now[:alert] = "error prohibited this book from being saved:"
+      render :edit
+    end
+  end
+
   def destroy
-    book = Book.find(params[:id]) 
+    book = Book.find( params[:id] )
     book.destroy
     redirect_to '/books'
-  end 
-  
+  end
+
   private
-  
+
   def book_params
     params.require(:book).permit(:title, :body)
-  end 
-  
+  end
+
 end
